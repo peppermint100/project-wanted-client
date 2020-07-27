f2ed92ddeca4573eaeacb3a9d828ba57a11cf5a3
peppermint100 <krpeppermint100@gmail.com>
2020-07-27 17:27:48 +0900

    redux settings

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { createStore } from "redux"
+import rootReducers from "./redux/reducers/rootReducers"
+import { Provider } from "react-redux"
+
+const store = createStore(rootReducers);
+
+store.subscribe(() => {
+  console.log("redux store : ", store.getState());
+});
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
